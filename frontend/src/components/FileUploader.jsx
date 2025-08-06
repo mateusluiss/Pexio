1213fc22545f866bd29f0749a7cd08f62de5458b
Teteo13 <mateusdev13@gmail.com>
2025-08-05 22:17:59 -0300

    Front-end responsivo e props

--- frontend/src/components/FileUploader.jsx
+++ frontend/src/components/FileUploader.jsx
@@ -1,27 +1,55 @@
 import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
-import { Upload } from "lucide-react";
+import { Upload, File } from "lucide-react";
 
-export default function FileUploader() {
-  const onDrop = useCallback((acceptedFiles) => {
-    console.log("Arquivos:", acceptedFiles);
-    // Aqui você pode salvar no estado, fazer upload, etc.
-  }, []);
+export default function FileUploader({
+  formato,
+  onArquivoSelecionado,
+  arquivoSelecionado,
+}) {
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      console.log("Arquivos:", acceptedFiles);
+      onArquivoSelecionado(acceptedFiles);
+      // Aqui você pode salvar no estado, fazer upload, etc.
+    },
+    [onArquivoSelecionado]
+  );
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
     <div
       {...getRootProps()}
-      className={`border-1 border-dashed h-full rounded-xl p-8 cursor-pointer transition flex place-items-center flex-col ${
+      className={`border-1 border-dashed rounded-xl p-8 cursor-pointer transition flex place-items-center flex-col mt-5 ${
         isDragActive ? "bg-blue-100 border-verde" : "bg-white border-gray-300"
       }`}
     >
-      <Upload size={45} color="#56828f" />
+      {arquivoSelecionado ? (
+        <File size={45} color="#56828f" />
+      ) : (
+        <Upload size={45} color="#56828f" />
+      )}
+
       <input {...getInputProps()} />
-      <p class="font-medium text-sm mt-3">Clique para escolher um arquivo</p>
-      <p class="text-xs text-gray-500 mt-2">ou arraste e solte aqui</p>
-      <p class="text-xs text-gray-500 mt-1">Formato aceito: PDF</p>
+      {arquivoSelecionado ? (
+        <div className="text-center">
+          <p className="font-medium text-sm mt-3">{arquivoSelecionado.name}</p>
+          <p className="text-xs text-gray-500 mt-2">
+            Clique para escolher outro arquivo
+          </p>
+        </div>
+      ) : (
+        <div className="text-center">
+          <p className="font-medium text-sm mt-3">
+            Clique para escolher um arquivo
+          </p>
+          <p className="text-xs text-gray-500 mt-2">ou arraste e solte aqui</p>
+          <p className="text-xs text-gray-500 mt-1">
+            Formato aceito: {formato}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/routes/Converter.jsx
+++ frontend/src/routes/Converter.jsx
@@ -2,61 +2,96 @@ import React from "react";
 import { ArrowLeft } from "lucide-react";
 import { Link } from "react-router";
 import FileUploader from "../components/FileUploader.jsx";
+import { useState } from "react";
 
 function Converter() {
+  const [formatoaceito, setFormatoaceito] = useState("PDF");
+  const [arquivoSelecionado, setArquivoSelecionado] = useState(null);
+
+  const handleChange = (event) => {
+    setFormatoaceito(event.target.value);
+  };
+
+  const handleArquivoSelecionado = (files) => {
+    if (files.length > 0) {
+      setArquivoSelecionado(files[0]);
+    } else {
+      setArquivoSelecionado(null);
+    }
+  };
+
   return (
     <>
-      <div class="h-screen place-items-center justify-center flex flex-col">
-        <div class=" w-[38rem] flex gap-4">
-          <Link
-            to="/"
-            class="flex rounded-[0.5rem] justify-center items-center pl-3 pr-3 gap-2 hover:bg-cianobranco transition-all ease-in-out duration-200"
-          >
-            <ArrowLeft color="#004766" size={15} />
-            <span class="text-cianotxt font-semibold text-xs">Voltar</span>
-          </Link>
-
-          <h1 class="text-cianotxt font-bold text-3xl">PEXIO</h1>
-        </div>
-        <div class="text-cianotxt mt-7 flex border-cianobranco shadow-xs border h-[30rem] w-[38rem] rounded-xl flex-col p-8">
-          <h2 class="font-medium">Escolha o tipo de conversão:</h2>
-          <ul class="grid grid-cols-2 gap-2 mt-5 ">
-            <li class="col-start-1 border border-gray-300 rounded p-3 flex gap-2">
-              <label
-                class="cursor-pointer flex gap-2 font-semibold text-xs"
-                for="pdf-png"
-              >
-                <input
-                  type="radio"
-                  id="pdf-png"
-                  class="cursor-pointer text-ciano"
-                  name="conversion"
-                ></input>
-                PDF → PNG
-              </label>
-            </li>
-
-            <li class="col-start-2 border border-gray-300 rounded p-3 flex gap-2">
-              <label
-                class="cursor-pointer flex gap-2 font-semibold text-xs"
-                for="png-pdf"
-              >
-                <input
-                  type="radio"
-                  id="png-pdf"
-                  class="cursor-pointer"
-                  name="conversion"
-                ></input>
-                PNG → PDF
-              </label>
-            </li>
-          </ul>
-
-          <h2 class="font-medium mt-8">Selecione seu arquivo:</h2>
-          <FileUploader />
-          <button class="bg-gradient-to-r from-ciano to-cianoclaro font-bold w-full text-white p-3 rounded-xl mt-7">
-            Converter PDF para PNG
-          </button>
+      <div className="h-screen place-items-center justify-center flex flex-col w-full">
+        <div>
+          <div className="flex gap-4 mx-auto md:w-[38rem]">
+            <Link
+              to="/"
+              className="flex rounded-[0.5rem] justify-center items-center pl-3 pr-3 gap-2 hover:bg-cianobranco transition-all ease-in-out duration-200"
+            >
+              <ArrowLeft color="#004766" size={15} />
+              <span className="text-cianotxt font-semibold text-xs">
+                Voltar
+              </span>
+            </Link>
+            <h1 className="text-cianotxt font-bold text-3xl">PEXIO</h1>
+          </div>
+          <div className="mx-auto mt-5 text-cianotxt flex border-cianobranco shadow-xs border max-h-fit rounded-xl flex-col p-5 md:h-[31rem] md:mt-7 md:w-[38rem] md:p-8">
+            <h2 className="font-medium">Escolha o tipo de conversão:</h2>
+            <ul className="grid grid-cols-2 gap-2 mt-5 ">
+              <li className="col-start-1 border border-gray-300 rounded p-3 flex gap-2">
+                <label
+                  className="cursor-pointer flex gap-2 font-semibold text-xs"
+                  htmlFor="pdf-png"
+                >
+                  <input
+                    type="radio"
+                    id="pdf-png"
+                    className="cursor-pointer text-ciano"
+                    name="conversion"
+                    value="PDF"
+                    onChange={handleChange}
+                    checked={formatoaceito === "PDF"}
+                  ></input>
+                  PDF → PNG
+                </label>
+              </li>
+              <li className="col-start-2 border border-gray-300 rounded p-3 flex gap-2">
+                <label
+                  className="cursor-pointer flex gap-2 font-semibold text-xs"
+                  htmlFor="png-pdf"
+                >
+                  <input
+                    type="radio"
+                    id="png-pdf"
+                    className="cursor-pointer"
+                    name="conversion"
+                    value="PNG"
+                    onChange={handleChange}
+                    checked={formatoaceito === "PNG"}
+                  ></input>
+                  PNG → PDF
+                </label>
+              </li>
+            </ul>
+            <h2 className="font-medium mt-8">Selecione seu arquivo:</h2>
+            <FileUploader
+              formato={formatoaceito}
+              onArquivoSelecionado={handleArquivoSelecionado}
+              arquivoSelecionado={arquivoSelecionado}
+            />
+            <button
+              disabled={!arquivoSelecionado}
+              className={`bg-gradient-to-r from-ciano to-cianoclaro font-bold w-full text-white p-3 rounded-xl mt-7 ${
+                !arquivoSelecionado
+                  ? "opacity-50 cursor-default"
+                  : "cursor-pointer"
+              }`}
+            >
+              Converter {formatoaceito} para{" "}
+              {formatoaceito === "PDF" ? "PNG" : "PDF"}
+            </button>
+          </div>
         </div>
       </div>
     </>
