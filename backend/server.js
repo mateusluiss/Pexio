776f82b4f49d6b36c5e5f239277899b37ec000a1
Teteo13 <mateusdev13@gmail.com>
2025-08-06 14:27:51 -0300

    Mudanças no back-end

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const sharp = require("sharp");
 const PDFDocument = require("pdfkit");
 const { PDFDocument: PDFLibDocument } = require("pdf-lib");
 const path = require("path");
+const { fromBuffer } = require("pdf2pic");
 
+const caminhoOutput = path.join(__dirname, "output");
 app.use(cors());
 
 const upload = multer({ storage: multer.memoryStorage() });
@@ -19,24 +21,31 @@ async function converter(formato, arquivos, res) {
     const totalPages = pdfDoc.getPageCount();
 
     console.log(`Total de páginas: ${totalPages}`);
+
+    const buffer = arquivos[0].buffer;
+    const options = {
+      density: 100,
+      saveFilename: "pagina",
+      savePath: "./output",
+      format: "png",
+      width: 800,
+      height: 1000,
+    };
   }
 }
 
 app.post("/upload", upload.any(), (req, res) => {
   console.log("Arquivos recebidos:", req.files);
-  res.json({ message: "Upload bem-sucedido!", total: req.files.length });
 
   const arquivos = req.files;
   const formato = arquivos[0].mimetype;
 
   converter(formato, arquivos, res);
 
-  if (formato === "application/pdf") {
-  }
+  // if (formato === "application/pdf") {
+  // }
 });
 
-app.get("/upload", (req, res) => {
-  res.send("Hello World");
-});
+app.get("/upload", (req, res) => {});
 
 app.listen(port, () => console.log("Rodando na porta", port));
